44cd45e2887ac9a2ce19b7158015b897fdb9dc42
samsad01 <46419166+samsad01@users.noreply.github.com>
2020-09-26 13:39:15 +0530

    First

---
+++ template.cpp
@@ -0,0 +1,50 @@
+/*
+*
+*	AUTHOR :- @soham01
+*		// "WORK HARD AND NEVER GIVE UP" //
+*
+*/
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update> //find_by_order(), order_of_key()
+#define pb push_back
+#define mp make_pair
+#define vi vector<long long int >
+#define vb vector<bool>
+#define vs vector<string>
+#define vd vector<double>
+#define vvi vector<vector<long long int> >
+#define vp vector<pair<long long int , long long int > >
+#define seti set<long long int >
+#define setc set<char>
+#define setd set<double>
+#define mod 1000000009
+#define endl '\n'
+typedef long long int lli;
+
+// ITS MY REQUEST TO PLEASE PLEASE DO LOOK AT THE CONSTRAINS GIVEN CAREFULLY
+// DO CHECK FOR CORNER CASES
+
+/*-------LET THE GAME BEGIN-----------*/
+
+void solve()
+{
+
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	lli t;
+	cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}
